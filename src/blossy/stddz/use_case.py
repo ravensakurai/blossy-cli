cf3011bebb3ac94dfdf5a9c97bb92fc0aa83154a
Ryan Sakurai <ryansakurai03@gmail.com>
2026-01-01 17:38:18 -0300

    refactor: remove dedundant try block

--- src/blossy/stddz/use_case.py
+++ src/blossy/stddz/use_case.py
@@ -47,31 +47,25 @@ class _StandardizeUseCaseOption1:
             raise typer.BadParameter("Negative starting number.")
 
         dir_abs_path = directory.expanduser().resolve()
+        files = self._get_files(dir_abs_path)
 
-        try:
-            files = self._get_files(dir_abs_path)
+        last_id = start_idx + len(files) - 1
+        min_qt_digits = len(str(last_id))
+        if min_qt_digits > qt_digits:
+            qt_digits = min_qt_digits
+            qt_readjusted = True
+        else:
+            qt_readjusted = False
 
-            last_id = start_idx + len(files) - 1
-            min_qt_digits = len(str(last_id))
-            if min_qt_digits > qt_digits:
-                qt_digits = min_qt_digits
-                qt_readjusted = True
-            else:
-                qt_readjusted = False
+        # to prevent overriding previous files
+        temp_prefix = "".join(choices(ascii_letters, k=10))
+        self._rename(dir_abs_path, files, temp_prefix, qt_digits, start_idx)
 
-            # to prevent overriding previous files
-            temp_prefix = "".join(choices(ascii_letters, k=10))
-            self._rename(dir_abs_path, files, temp_prefix, qt_digits, start_idx)
+        files = self._get_files(dir_abs_path)
+        self._rename(dir_abs_path, files, prefix, qt_digits, start_idx)
 
-            files = self._get_files(dir_abs_path)
-            self._rename(dir_abs_path, files, prefix, qt_digits, start_idx)
-
-            if qt_readjusted:
-                print("Quantity of digits had to be readjusted.")
-        except FileNotFoundError as e:
-            raise typer.BadParameter(f"'{dir_abs_path}' does not exist.") from e
-        except NotADirectoryError as e:
-            raise typer.BadParameter(f"'{dir_abs_path}' is not a directory.") from e
+        if qt_readjusted:
+            print("Quantity of digits had to be readjusted.")
 
     def _get_files(self, directory_path: Path) -> tuple[Path, ...]:
         files = []
